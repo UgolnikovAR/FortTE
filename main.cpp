6da20cef2aecbb0a6d4e2d6027c92d1345ecba65
Ugolnikov Anton Romanovich <a.r.ugolnikov@gmail.com>
2020-07-04 12:30:18 +0300

    designed README.txt with description a programm using

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
-#include <iostream>
+//#include <iostream>
 #include <conio.h>
+#include "DataClass.cpp"
 
 int main()
 {
-   // std::cout << "hi";
+    DataClass data;
+
+    data.field = "Hello1\0";
+    data.field.print('\r');
+
+    data.field = 126;
+    data.field.print('\r');
+
+    data.field = float(89.7);
+    data.field.print('\r');
+
+    system("pause");
     return 0;
 }
 
 
-//ch = getch
+//ch = getch()
 //system("cls");
